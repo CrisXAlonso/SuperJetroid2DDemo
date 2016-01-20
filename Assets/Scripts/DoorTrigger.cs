60d2c154edaa65447b210495e07213e03dea8931
Cristian Alonso <crxalonso@gmail.com>
2016-01-20 18:00:43 -0500

    Chapter 4 Completed. The player now has object with which to interact.

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+
+	public Door door;
+	public bool ignoreTrigger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D target) {
+
+		if (ignoreTrigger) {
+			return;
+		}
+
+
+		if (target.gameObject.tag == "Player") {
+			door.Open ();
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D target) {
+
+		if (ignoreTrigger) {
+			return;
+		}
+
+		if (target.gameObject.tag == "Player") {
+			door.Close ();
+		}
+	}
+
+	public void Toggle(bool value){
+		if (value) {
+			door.Open ();
+
+		} else {
+			door.Close ();
+		}
+	}
+
+	public void OnDrawGizmos () {
+		Gizmos.color = ignoreTrigger ? Color.gray : Color.green;
+
+		var bc2D = GetComponent<BoxCollider2D> ();
+		var bc2DPos = bc2D.transform.position;
+		var newPos = new Vector2 (bc2DPos.x + bc2D.offset.x, bc2DPos.y + bc2D.offset.y);
+		Gizmos.DrawWireCube (newPos, new Vector2(bc2D.size.x, bc2D.size.y));
+	}
+}
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	public DoorTrigger[] doorTriggers;
+	private Animator animator;
+	public bool sticky;
+	private bool down;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D target){
+		animator.SetInteger ("AnimState", 1);
+		down = true;
+		foreach (DoorTrigger trigger in doorTriggers) {
+			if (trigger != null) {
+				trigger.Toggle (true);
+			}
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D target){
+		animator.SetInteger ("AnimState", 2);
+		if (sticky && down)
+			return;
+
+		down = false;
+		foreach (DoorTrigger trigger in doorTriggers) {
+			if (trigger != null) {
+				trigger.Toggle (false);
+			}
+
+		}
+	}
+
+	void OnDrawGizmos () {
+		Gizmos.color = sticky ? Color.red : Color.green;
+		foreach (DoorTrigger trigger in doorTriggers) {
+			if (trigger != null) {
+				Gizmos.DrawLine (transform.position, trigger.door.transform.position);
+			}
+
+		}
+	}
+}
